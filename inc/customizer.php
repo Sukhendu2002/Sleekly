c5696a3776b662cc64eeddb4c851af93e3f52615
Sukhendu Sekhar Guria <sukhenduguria2017@gmail.com>
2023-12-30 11:27:37 +0530

    Add socials using the customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -31,6 +31,96 @@ function sleekly_customize_register( $wp_customize ) {
 			)
 		);
 	}
+
+	$wp_customize->add_section(
+		'sleekly_footer_section',
+		array(
+			'title'       => __( 'Footer', 'sleekly' ),
+			'priority'    => 30,
+			'description' => __( 'Footer section', 'sleekly' ),
+		)
+	);
+
+	$wp_customize->add_setting(
+		'sleekly_footer_text',
+		array(
+			'default'           => __( 'Footer text', 'sleekly' ),
+			'sanitize_callback' => 'sanitize_text_field',
+		)
+	);
+
+	$wp_customize->add_control(
+		'sleekly_footer_text',
+		array(
+			'label'    => __( 'Footer text', 'sleekly' ),
+			'section'  => 'sleekly_footer_section',
+			'type'     => 'text',
+			'priority' => 1,
+		)
+	);
+
+	$wp_customize->add_section(
+		'sleekly_social_section',
+		array(
+			'title'       => __( 'Social', 'sleekly' ),
+			'priority'    => 30,
+			'description' => __( 'Social section', 'sleekly' ),
+		)
+	);
+
+	$wp_customize->add_setting(
+		'sleekly_facebook_url',
+		array(
+			'default'           => __( 'Facebook URL', 'sleekly' ),
+			'sanitize_callback' => 'sanitize_text_field',
+		)
+	);
+
+	$wp_customize->add_control(
+		'sleekly_facebook_url',
+		array(
+			'label'    => __( 'Facebook URL', 'sleekly' ),
+			'section'  => 'sleekly_social_section',
+			'type'     => 'text',
+			'priority' => 1,
+		)
+	);
+
+	$wp_customize->add_setting(
+		'sleekly_twitter_url',
+		array(
+			'default'           => __( 'Twitter URL', 'sleekly' ),
+			'sanitize_callback' => 'sanitize_text_field',
+		)
+	);
+
+	$wp_customize->add_control(
+		'sleekly_twitter_url',
+		array(
+			'label'    => __( 'Twitter URL', 'sleekly' ),
+			'section'  => 'sleekly_social_section',
+			'type'     => 'text',
+			'priority' => 2,
+		)
+	);
+
+	$wp_customize->add_setting(
+		'sleekly_instagram_url',
+		array(
+			'default'           => __( 'Instagram URL', 'sleekly' ),
+			'sanitize_callback' => 'sanitize_text_field',
+		)
+	);
+
+	$wp_customize->add_control(
+		'sleekly_instagram_url',
+		array(
+			'label'    => __( 'Instagram URL', 'sleekly' ),
+			'section'  => 'sleekly_social_section',
+			'type'     => 'text',
+			'priority' => 3,
+		)
+	);
 }
 add_action( 'customize_register', 'sleekly_customize_register' );
 
